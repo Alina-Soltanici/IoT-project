232ad69ebf34fd0ea870a7a40c4689b5b090ec46
Alina <alina@Alinas-MacBook-Pro.local>
2025-12-01 17:10:03 +0200

    Add SensorDeviceRequest DTO for sending sensor device details (name and location) from client to server.

---
+++ src/main/java/com/example/IoT_project/dto/SensorDeviceRequest.java
@@ -0,0 +1,10 @@
+// SensorDeviceRequest.java
+package com.example.IoT_project.dto;
+
+import lombok.Data;
+
+@Data
+public class SensorDeviceRequest {
+    private String sensorName;
+    private String location;
+}
